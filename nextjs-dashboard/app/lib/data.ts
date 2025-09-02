0fa5ec4c8daea25834b1f256d1a92be72b751ca7
jambrizz <ambrizjovani@gmail.com>
2025-09-01 20:47:29 -0700

    commented out the fetchProviders function to resolve deploy fail

--- nextjs-dashboard/app/lib/data.ts
+++ nextjs-dashboard/app/lib/data.ts
@@ -7,8 +7,9 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 export async function fetchProviders() {
     try {
-        const data = await sql<providers[]>`SELECT * FROM providers`;
-        return data;
+        //Fix this section below
+        //const data = await sql<providers[]>`SELECT * FROM providers`;
+        //return data;
     } catch (error) {
         console.error('Database Error:', error);
         throw new Error('Failed to fetch Provider data.');
