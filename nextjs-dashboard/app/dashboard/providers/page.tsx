f3e275d9a752569061d36c145a4676625ae98e85
jambrizz <ambrizjovani@gmail.com>
2025-09-18 00:22:20 -0700

    deploy failure fix attempt 6

--- nextjs-dashboard/app/dashboard/providers/page.tsx
+++ nextjs-dashboard/app/dashboard/providers/page.tsx
@@ -4,6 +4,47 @@ import { fetchProviders, fetchProvidersPages } from '@/app/lib/data';
 import ProviderList from '@/app/components/ProviderList';
 import { Suspense } from 'react';
 
+export default async function ProvidersPage({
+    searchParams,
+}: {
+    searchParams?: {
+        page?: string;
+        query?: string;
+    };
+}) {
+    const currentPage = Number(searchParams?.page || '1');
+    const query = searchParams?.query || '';
+
+    const providers = await fetchProviders(currentPage, query);
+    const totalPages = await fetchProvidersPages(query);
+
+    return (
+        <main className="p-6">
+            <div className="mb-6">
+                <h1 className="text-3xl font-bold text-gray-800">List of Providers</h1>
+            </div>
+
+            <div>
+                <Search placeholder="Search Provider" />
+            </div>
+
+            <Suspense key={query + currentPage}>
+                <ProviderList initialProviders={providers} />
+            </Suspense>
+
+            <Pagination totalPages={totalPages} currentPage={currentPage} query={query} />
+        </main>
+    );
+}
+
+
+/*
+import Pagination from '@/app/ui/support/pagination';
+import Search from '@/app/ui/search';
+import { fetchProviders, fetchProvidersPages } from '@/app/lib/data';
+import ProviderList from '@/app/components/ProviderList';
+import { Suspense } from 'react';
+
 type Props = {
     searchParams?: Record<string, string | string[] | undefined>;
 };
@@ -38,3 +79,4 @@ export default async function ProvidersPage({ searchParams }: Props) {
         </main>
     );
 }
+*/
