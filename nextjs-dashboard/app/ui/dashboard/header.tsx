11646c4672ca5ce07354fa4b8365a430236e13dd
jambrizz <ambrizjovani@gmail.com>
2025-09-23 07:09:58 -0700

    adding color configuration for tailwind to implement new color palette. TODO Query all providers into database

--- nextjs-dashboard/app/ui/dashboard/header.tsx
+++ nextjs-dashboard/app/ui/dashboard/header.tsx
@@ -15,10 +15,13 @@ const links = [
     { name: 'Login', href: '/dashboard/login', icon: UserCircleIcon }
 ];
 
+//bg-gray-100
+//bg-gray-50
+
 export default function NavLinks() {
     const pathname = usePathname();
     return (
-        <nav className="flex w-full items-center justify-between bg-gray-100 px-6 py-3 shadow-md">
+        <nav className="bg-harmony-lazuli flex w-full items-center justify-between px-6 py-3 shadow-md">
             <div className="flex items-center gap-8">
                 {links.map((link) => {
                     const LinkIcon = link.icon;
@@ -28,7 +31,7 @@ export default function NavLinks() {
                             href={link.href}
                             //className="flex items-center gap-2 text-gray-700 transition-colors hover:text-blue-600"
                             className={clsx(
-                                'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+                                'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-300 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
                                 {
                                     'bg-sky-100 text-blue-600': pathname === link.href,
                                 },
--- nextjs-dashboard/tailwind.config.ts
+++ nextjs-dashboard/tailwind.config.ts
@@ -16,6 +16,17 @@ const config: Config = {
           400: '#2589FE',
           500: '#0070F3',
           600: '#2F6FEB',
+
+        },
+        navy: {
+          600: '#2C2350',
+        },
+        harmony: {
+            lazuli: '#05668D',
+            teal: '#028090',
+            persian: '#00A896',
+            mint: '#02C39A',
+            cream: '#F0F3BD',
         },
       },
     },
